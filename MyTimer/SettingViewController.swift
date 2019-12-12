da200342ddee410952cd450969133434d450635e
githabpractice <nightmare157kyuzou@gmail.com>
2019-12-13 00:44:19 +0900

    view Controller コード追加

---
+++ MyTimer/SettingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingViewController.swift
+//  MyTimer
+//
+//  Created by taka on 2019/12/12.
+//  Copyright © 2019 Swift-mytimer. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBOutlet weak var timerSettingPicker: UIPickerView!
+    
+    @IBAction func decisionButtonAction(_ sender: Any) {
+    }
+    
+}
